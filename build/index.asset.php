caa2277d2ec03bbb2ebe2626a0e986e138b9529c
Akiya <autumnsky64@gmail.com>
2020-12-25 06:24:45 +0900

    Improve style

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'fb2362d0de517a5af2f487e4efcb7e24');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9a68fa06b98f7ef3b5b0b74440cab642');
